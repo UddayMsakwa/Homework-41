12215e752b56ae946af0f5f7acdc8885382a0802
Udday Msakwa <uddaymsakwa@gmail.com>
2026-06-15 17:12:38 +0700

    there was a magic numver of 500 in the RepairOrder file which I fixed by refactoring Magic Numbers using the Replace Magic Number with Constant.

---
+++ Task3/AutoServiceApp/Models/BusinessRules.cs
@@ -0,0 +1,7 @@
+﻿namespace AutoServiceApp.Models
+{
+    public static class BusinessRules
+    {
+        public const decimal UrgentRepairFee = 500m;
+    }
+}
--- Task3/AutoServiceApp/Models/RepairOrder.cs
+++ Task3/AutoServiceApp/Models/RepairOrder.cs
@@ -33,7 +33,7 @@ public class RepairOrder : BaseEntity
 public class UrgentRepairOrder : RepairOrder
 {
     public bool NeedTaxi { get; set; }
-    public decimal UrgentFee { get; set; } = 500;
+    public decimal UrgentFee { get; set; } = BusinessRules.UrgentRepairFee;
 }
 
 public class WarrantyRepairOrder : RepairOrder
